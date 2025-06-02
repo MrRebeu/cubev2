a581943e682a079dc2500dd9681dba1a6107ea04
Mrrebeu <abdallah.khefif@gmail.com>
2025-06-02 13:48:04 +0200

    222

--- enemy/enemy_core_2.c
+++ enemy/enemy_core_2.c
@@ -1,40 +1,55 @@
 #include "cube3d.h"
 
-int	line_of_sight(double ex, double ey, double px, double py, t_map *map)
+static void	init_los_data(t_los_data *d, double ex,
+		double ey, double px, double py)
 {
 	double	dx;
 	double	dy;
-	double	distance;
-	double	step_x;
-	double	step_y;
-	double	x;
-	double	y;
-	double	traveled;
-	int		map_x;
-	int		map_y;
 
 	dx = px - ex;
 	dy = py - ey;
-	distance = sqrt(dx * dx + dy * dy);
-	step_x = (dx / distance) * 5.0;
-	step_y = (dy / distance) * 5.0;
-	x = ex;
-	y = ey;
-	traveled = 0.0;
-	while (traveled < distance)
+	d->distance = sqrt(dx * dx + dy * dy);
+	d->step_x = (dx / d->distance) * 5.0;
+	d->step_y = (dy / d->distance) * 5.0;
+	d->x = ex;
+	d->y = ey;
+	d->traveled = 0.0;
+}
+
+static int	check_wall_hit(t_los_data *d, t_map *map)
+{
+	int	map_x;
+	int	map_y;
+
+	map_x = (int)(d->x / TILE_SIZE);
+	map_y = (int)(d->y / TILE_SIZE);
+	if (map_x < 0 || map_x >= map->width || map_y < 0 || map_y >= map->height)
+		return (1);
+	return (map->matrix[map_y][map_x] == '1');
+}
+
+int	line_of_sight(double ex, double ey, double px, double py, t_map *map)
+{
+	t_los_data	d;
+
+	init_los_data(&d, ex, ey, px, py);
+	while (d.traveled < d.distance)
 	{
-		map_x = (int)(x / TILE_SIZE);
-		map_y = (int)(y / TILE_SIZE);
-		if (map_x < 0 || map_x >= map->width ||
-			map_y < 0 || map_y >= map->height)
+		if (check_wall_hit(&d, map))
 			return (0);
-		if (map->matrix[map_y][map_x] == '1')
-			return (0);
-		x += step_x;
-		y += step_y;
-		traveled += 5.0;
+		d.x += d.step_x;
+		d.y += d.step_y;
+		d.traveled += 5.0;
 	}
 	return (1);
+}
+
+static void	handle_enemy_death(t_enemy *enemy)
+{
+	enemy->death_timer = 300;
+	enemy->animation.current_frame = 0;
+	enemy->animation.frame_counter = 0;
+	enemy->state = DEAD;
 }
 
 int	damage_enemy_at_position(t_game *game, int tile_x, int tile_y, int damage)
@@ -55,14 +70,10 @@ int	damage_enemy_at_position(t_game *game, int tile_x, int tile_y, int damage)
 			enemy->health -= damage;
 			if (enemy->health <= 0)
 			{
-				enemy->death_timer = 300;
-				enemy->animation.current_frame = 0;
-				enemy->animation.frame_counter = 0;
-				enemy->state = DEAD;
+				handle_enemy_death(enemy);
 				return (1);
 			}
-			else
-				return (0);
+			return (0);
 		}
 		i++;
 	}
